4172af33618c4f67426c2c8627e04cca1833977a
huayun1998 <3477767986@qq.com>
2020-02-19 09:58:54 +0800

    头部

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import facstclick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
+facstclick.attach(document.body)
 
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App)
 }).$mount('#app')
